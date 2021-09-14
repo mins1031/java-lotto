0c1a91cd72b52f548383877a9753cae4c0caf6b9
minyoung <urisegea@naver.com>
2021-09-14 14:34:47 +0900

    refactor: 자동생성 조금 수정

--- src/main/java/LottoUtil.java
+++ src/main/java/LottoUtil.java
@@ -21,18 +21,14 @@ public class LottoUtil {
 
     public static List<LottoNumber> autoNumCreate(int autoLottoCount) {
         List<LottoNumber> autoLottoList = new ArrayList<>();
-        Random random = new Random();
-        Set<Integer> temp = new HashSet<>();
+
         for (int i = 0 ; i < autoLottoCount; i++){
             LottoNumber lottoNumber = new LottoNumber();
+            List<Integer> autoNumbers = getRandomNumList();
 
-            for (Integer integer : getRandomNumList()) {
+            for (Integer integer : autoNumbers) {
                 lottoNumber.add(integer);
             }
-
-            /*for (int j = 0; j < 6; j++){
-                lottoNumber.add(random.nextInt(45)+1);
-            }*/
             autoLottoList.add(lottoNumber);
         }
         return autoLottoList;
@@ -46,19 +42,8 @@ public class LottoUtil {
         }
 
         List<Integer> nonDupList = new ArrayList<>(temp);
-        //Collections.sort(nonDupList);
 
         return nonDupList;
     }
-//    public static List<LottoNumber> manualNumParser (List<String> manualList) {
-//        List<LottoNumber> result = new ArrayList<>();
-//
-//        for (String manual : manualList) {
-//            LottoNumber lotto = new LottoNumber();
-//            lotto.addManual(manual.split(", "));
-//            result.add(lotto);
-//        }
-//
-//        return result;
-//    }
+
 }
