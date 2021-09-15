af0da0c11db387a0a14dd04670a7dab096e85ad6
minyoung <urisegea@naver.com>
2021-09-15 16:31:53 +0900

    feat: input scanner 작성

--- src/main/java/InputScanner.java
+++ src/main/java/InputScanner.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InputScanner {
@@ -5,14 +6,25 @@ public class InputScanner {
     private static Scanner scan = new Scanner(System.in);
 
     public static int scanInteger(){
-
-        int input = scan.nextInt();
-
+        int input = 0;
+        try {
+            input = scan.nextInt();
+            if (input <= 0){
+                generateInputError();
+            }
+        } catch (RuntimeException e) {
+            generateInputError();
+        }
         return input;
     }
 
     public String scanString(){
         return scan.nextLine();
+    }
+
+    private static void generateInputError(){
+        System.out.println("잘못된 입력입니다. 프로그램을 다시 실행해주세요");
+        System.exit(1);
     }
 
 
---
+++ src/main/java/domain/LottoNo.java
@@ -0,0 +1,9 @@
+package domain;
+
+public class LottoNo {
+    private final int number;
+
+    public LottoNo(int number) {
+        this.number = number;
+    }
+}
